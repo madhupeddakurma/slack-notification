d4d07f7f6abb1f8569b1dcf37d3a67670d8a34ce
madhu <kurmamadhu45@gmail.com>
2026-04-01 13:17:35 +0530

    main modified

--- src/main/java/com/commit/tracker/TrackerApplication.java
+++ src/main/java/com/commit/tracker/TrackerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrackerApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(TrackerApplication.class, args);
 	}
 }
